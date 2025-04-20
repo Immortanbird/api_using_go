d4b068be86c3ef2bef9609e6c7134ecb0ca4cac5
Yiming <zhou_yiming0109@outlook.com>
2025-04-20 23:43:28 +0900

    minor change

--- crud/init.go
+++ crud/init.go
@@ -11,7 +11,7 @@ import (
 
 var db *gorm.DB
 
-func OpenDB() *gorm.DB {
+func OpenDB() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		os.Getenv("db.user"),
 		os.Getenv("db.pwd"),
@@ -20,12 +20,11 @@ func OpenDB() *gorm.DB {
 		os.Getenv("db.name"),
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		zap.L().Panic("Failed to connect to database.")
 	}
 
 	zap.L().Info("Database connected.")
-
-	return db
 }
