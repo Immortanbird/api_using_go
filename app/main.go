d4be7b9af0caf862712e39c15bf8a5499566a14c
Yiming <zhou_yiming0109@outlook.com>
2025-04-20 23:32:02 +0900

    minor change

--- app/main.go
+++ app/main.go
@@ -1,51 +1,27 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/Immortanbird/api_using_go/config"
 	"github.com/Immortanbird/api_using_go/handler"
-	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
-	"go.uber.org/zap"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	config.LoadEnv()
+	config.LoadLogger()
 }
 
 func main() {
-	config.Init("config.json", "json", "./config")
-
-	// var logger *zap.Logger
-	// switch viper.GetString("mode") {
-	// case "debug":
-	// 	logger, _ = zap.NewDevelopment()
-	// case "test":
-	// 	logger = zap.NewExample()
-	// case "prod":
-	// 	logger, _ = zap.NewProduction()
-	// default:
-	// 	logger, _ = zap.NewProduction()
-	// }
-	logger, _ := zap.NewDevelopment()
-	// defer logger.Sync()
-
-	logger.Info("logger created", zap.String("sugar", "no"))
-
 	// g := gin.Default()
 	g := gin.New()
-	gin.SetMode(viper.GetString("mode"))
+	gin.SetMode(os.Getenv("mode"))
 
 	handler.Load(g)
 
-	g.Run("localhost:" + viper.GetString("port"))
+	g.Run(os.Getenv("addr") + ":" + os.Getenv("port"))
 }
