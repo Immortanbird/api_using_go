ab8a311227712a800862a0be01b5412e7a74f42a
Yiming Zhou <zhou_yiming0109@outlook.com>
2025-06-01 15:07:12 +0900

    feat/cors

--- api/v0.1/route.go
+++ api/v0.1/route.go
@@ -2,13 +2,33 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/Immortanbird/api_using_go/api/v0.1/handler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
 func RegisterMiddleware(g *gin.Engine, mw ...gin.HandlerFunc) {
+	// Define your CORS configuration
+	config := cors.Config{
+		// AllowOrigins: []string{"http://localhost:8080", "https://your-vue-app.com"}, // Frontend URLs
+		AllowOrigins:     []string{"*"}, // Allows all origins. For production, list specific origins.
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"}, // Headers the browser is allowed to access
+		AllowCredentials: true,                       // Important for cookies, authorization headers with HTTPS
+		// AllowOriginFunc: func(origin string) bool { // For more complex origin checking
+		//  return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour, // How long the results of a preflight request can be cached
+	}
+
+	g.Use(cors.New(config))
+
+	zap.L().Info("CORS configured.")
+
 	// Middlewares.
 	g.Use(gin.Recovery())
 	g.Use(gin.Logger())
