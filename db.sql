c3b430cdbeee4124e7573637f3b69987ee4ec0c6
Yiming Zhou <zhou_yiming0109@outlook.com>
2025-07-17 00:27:08 +0900

    fix: incorrect database id type and unaligned gorm model fields

--- db.sql
+++ db.sql
@@ -1,9 +1,7 @@
 CREATE TABLE refresh_tokens (
-    -- The unique ID for this specific token/session record.
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID(), 1)),
 
-    -- Foreign key to your users table. If a user is deleted, their tokens are too.
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id BINARY(16) NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 
     -- The unique ID from the JWT's "jti" claim. This is what you'll use to look it up.
     token_jti TEXT NOT NULL UNIQUE,
@@ -13,10 +11,10 @@ CREATE TABLE refresh_tokens (
 
     -- The token's expiration date from the JWT's "exp" claim.
     -- Storing it here allows for efficient cleanup of expired tokens.
-    expires_at TIMESTAMPTZ NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
 
     -- The timestamp when this record was created.
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 
     -- (Optional but Recommended) Additional metadata for session management.
     ip_address TEXT,
@@ -24,7 +22,7 @@ CREATE TABLE refresh_tokens (
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
-	`id` BINARY(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`id` BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID(), 1)),
 	`user_name` VARCHAR(20) NOT NULL UNIQUE,
 	`email` VARCHAR(128) NOT NULL UNIQUE,
 	`pwd_hash` VARCHAR(255) NOT NULL,
